c07fcb1a7c042bca8d050c0e0f1fab7f9b677251
urboyfriendfavgirl <108607538+urboyfriendfavgirl@users.noreply.github.com>
2022-09-05 13:42:53 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "hewo",
+  text2: "phải nói thậc đó",
+  text3: "mình bíc b iu mình mà đk",
+  text4: "chọn kh là chos",
+  text5: "iu",
+  text6: "kh iu",
+  text7: "lí do thích mình kk",
+  text8: "gửi",
+  text9: "vì mình siu cấp vjp pro",
+  text10: "mình bíc mà iu b hé hé",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "hết r đó b-",
+  text12: "Tiếp tục",
 };
 
 $(document).ready(function () {
